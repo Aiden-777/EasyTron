c3f619df77515ce426ae0bfa91c8bcb78b24bfdd
Aiden-777 <Aiden>
2022-11-02 19:32:15 +0800

    测试用例更新-方便查阅

--- core/src/test/java/org/tron/easywork/LocalTransferTest.java
+++ core/src/test/java/org/tron/easywork/LocalTransferTest.java
@@ -3,14 +3,13 @@ package org.tron.easywork;
 import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.GeneratedMessageV3;
-import com.google.protobuf.InvalidProtocolBufferException;
 import lombok.extern.slf4j.Slf4j;
 import org.bouncycastle.util.encoders.Hex;
 import org.junit.Test;
 import org.tron.easywork.demo.DemoLocalTransferHandler;
-import org.tron.easywork.exception.SmartParamDecodeException;
-import org.tron.easywork.factory.ApiWrapperFactory;
-import org.tron.easywork.handler.transfer.*;
+import org.tron.easywork.handler.transfer.LocalTransferContext;
+import org.tron.easywork.handler.transfer.Trc20TransferHandler;
+import org.tron.easywork.handler.transfer.TrxTransferHandler;
 import org.tron.easywork.model.*;
 import org.tron.easywork.util.BlockParser;
 import org.tron.easywork.util.ContractUtils;
@@ -34,12 +33,57 @@ import java.util.Date;
 import java.util.List;
 
 /**
+ * 本地转账交易
+ *
  * @author Admin
  * @version 1.0
  * @time 2022-10-29 18:18
  */
 @Slf4j
 public class LocalTransferTest extends BaseTest {
+
+    /**
+     * 从 LocalTransferContext 执行转账 （可实用级别）
+     * 引入 LocalTransferContext ，可以不用在意传入的转账是什么类型，程序自动识别
+     * <p>
+     * 1273a14d335dbfb47a003b3ac92192488fe135f5a7babe14ff3454b47262aadf
+     */
+    @Test
+    public void localTransferContext() throws IllegalException {
+        // 转出地址
+        String fromAddress = fromAccount.getBase58CheckAddress();
+        // 实际转账金额
+        BigDecimal realAmount = BigDecimal.valueOf(1);
+        // 合约
+        Trc20ContractInfo trc20ContractInfo = ContractUtils.readTrc20ContractInfo(testContractAddress, wrapper);
+        // 系统转账金额
+        BigDecimal transferAmount = trc20ContractInfo.getTransferAmount(realAmount);
+        // 构造trc20交易
+        Trc20TransferInfo trc20TransferInfo = new Trc20TransferInfo(fromAddress, toAddress, transferAmount, trc20ContractInfo.getAddress());
+        // 备注 - 可以为空
+        trc20TransferInfo.setMemo("大聪明...");
+        // 矿工费限制 - 可以为空，处理器默认设置为10trx
+        trc20TransferInfo.setFeeLimit(defaultFeeLimit);
+        // 获取参考区块
+        Chain.Block refBlock = wrapper.getNowBlock();
+        // 处理器上下文
+        LocalTransferContext localTransferContext = new LocalTransferContext();
+        // 添加trc20处理器
+        localTransferContext.addHandler("trc20TransferHandler", new Trc20TransferHandler());
+        // 添加trx处理器
+        localTransferContext.addHandler("trxTransferHandler", new TrxTransferHandler());
+        try {
+            // 构造本地交易
+            Chain.Transaction transaction = localTransferContext.buildLocalTransfer(trc20TransferInfo, refBlock.getBlockHeader());
+            // 签名
+            Chain.Transaction signTransaction = wrapper.signTransaction(transaction);
+            // 广播并返回ID
+            String tid = wrapper.broadcastTransaction(signTransaction);
+            log.debug(tid);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     /**
      * # 333 - 完整的本地交易构造（参考）
@@ -231,143 +275,6 @@ public class LocalTransferTest extends BaseTest {
         Chain.Transaction transaction = trxTransferHandler.buildLocalTransfer(transferInfo, refBlock.getBlockHeader());
         // 签名
         Chain.Transaction signTransaction = wrapper.signTransaction(transaction);
-        // 广播并返回ID
-        String tid = wrapper.broadcastTransaction(signTransaction);
-        log.debug(tid);
-    }
-
-    /**
-     * 从 LocalTransferContext 执行转账 （可实用级别）
-     * 1273a14d335dbfb47a003b3ac92192488fe135f5a7babe14ff3454b47262aadf
-     */
-    @Test
-    public void localTransferContext() throws IllegalException {
-        // 转出地址
-        String fromAddress = fromAccount.getBase58CheckAddress();
-        // 实际转账金额
-        BigDecimal realAmount = BigDecimal.valueOf(1);
-        // 合约
-        Trc20ContractInfo trc20ContractInfo = ContractUtils.readTrc20ContractInfo(testContractAddress, wrapper);
-        // 系统转账金额
-        BigDecimal transferAmount = trc20ContractInfo.getTransferAmount(realAmount);
-        // 构造trc20交易
-        Trc20TransferInfo trc20TransferInfo = new Trc20TransferInfo(fromAddress, toAddress, transferAmount, trc20ContractInfo.getAddress());
-        // 备注 - 可以为空
-        trc20TransferInfo.setMemo("大聪明...");
-        // 矿工费限制 - 可以为空，处理器默认设置为10trx
-        trc20TransferInfo.setFeeLimit(defaultFeeLimit);
-        // 获取参考区块
-        Chain.Block refBlock = wrapper.getNowBlock();
-        // 处理器上下文
-        LocalTransferContext localTransferContext = new LocalTransferContext();
-        // 添加trc20处理器
-        localTransferContext.addHandler("trc20TransferHandler", new Trc20TransferHandler());
-        // 添加trx处理器
-        localTransferContext.addHandler("trxTransferHandler", new TrxTransferHandler());
-        try {
-            // 构造本地交易
-            Chain.Transaction transaction = localTransferContext.buildLocalTransfer(trc20TransferInfo, refBlock.getBlockHeader());
-            // 签名
-            Chain.Transaction signTransaction = wrapper.signTransaction(transaction);
-            // 广播并返回ID
-            String tid = wrapper.broadcastTransaction(signTransaction);
-            log.debug(tid);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * 读取主网最新区块的转账交易内容
-     */
-    @Test
-    public void readBlockTest() throws IllegalException {
-        // 此处使用主网 - 测试网读取区块看不出效果，交易数量太少
-        wrapper = ApiWrapperFactory.create(ApiWrapperFactory.NetType.Mainnet, privateKey, apiKey);
-        // 获取最新块
-        Chain.Block nowBlock = wrapper.getNowBlock();
-
-        // trx转账处理器
-        TrxTransferHandler trxTransferHandler = new TrxTransferHandler();
-        // trc10转账处理器
-        Trc10TransferHandler trc10TransferHandler = new Trc10TransferHandler();
-        // trc20转账处理器
-        Trc20TransferHandler trc20TransferHandler = new Trc20TransferHandler();
-
-        // 转账处理器上下文
-        LocalTransferContext localTransferContext = new LocalTransferContext();
-        localTransferContext.addHandler("trxTransferHandler", trxTransferHandler);
-        localTransferContext.addHandler("trc10TransferHandler", trc10TransferHandler);
-        localTransferContext.addHandler("trc20TransferHandler", trc20TransferHandler);
-
-        // 遍历交易列表
-        nowBlock.getTransactionsList().forEach(transaction -> {
-            // 根据交易合约类型获取处理器
-            BaseTransferHandler handler = localTransferContext.getHandler(transaction.getRawData().getContract(0).getType());
-            if (null == handler) {
-                return;
-            }
-            try {
-                // 解析交易，不忽略失败的交易
-                TransferInfo transfer = handler.parse(transaction);
-                log.debug("状态：{}，类型：{}\t到账地址：{}\t金额：{}", transfer.getStatus(), transfer.getTransferType(), transfer.getTo(), transfer.getAmount());
-                // 其他逻辑 ................
-            } catch (InvalidProtocolBufferException e) {
-                log.error("解包错误：{}", e.getMessage());
-            } catch (SmartParamDecodeException e) {
-                log.error("转账解析错误：{}", e.getMessage());
-            } catch (UnsupportedOperationException e) {
-                // 不支持的操作,如非trc20转账
-            } catch (Exception e) {
-                log.error("异常兜底：{}", e.getMessage());
-                e.printStackTrace();
-            }
-        });
-    }
-
-    /**
-     * 本地多签转账
-     * shasta网络例子：
-     * 296fc6ae7c8a61c0005b64d38b51c99623fb7475277ab2bbc0439b07f7a86afe
-     */
-    // @Test
-    public void multiSignature() throws IllegalException {
-        // TKjPqKq77777FPKUdLRMPNUWtU4jNEpUQF   主账号，业务所致此处未用到私钥
-        AccountInfo account = new AccountInfo("");
-
-        // TBjxJTNwZeaKrbHyDum5Rwj1xU99999n8Z   Trx转账权限
-        AccountInfo account1 = new AccountInfo("");
-
-        // TEczEK6uzD88888QhstH6QDwB167ZsXPrb   Trx转账权限
-        AccountInfo account2 = new AccountInfo("");
-
-        // 到账地址
-        String fromAddress = account.getBase58CheckAddress();
-        // 实际转账金额
-        BigDecimal realAmount = BigDecimal.valueOf(1);
-        // 合约
-        Trc20ContractInfo trc20ContractInfo = ContractUtils.readTrc20ContractInfo(testContractAddress, wrapper);
-        // 系统转账金额
-        BigDecimal transferAmount = trc20ContractInfo.getTransferAmount(realAmount);
-        // trc20交易
-        Trc20TransferInfo transferInfo = new Trc20TransferInfo(fromAddress, toAddress, transferAmount, trc20ContractInfo.getAddress());
-        // 矿工费限制
-        transferInfo.setFeeLimit(Convert.toSun(BigDecimal.valueOf(20), Convert.Unit.TRX).longValue());
-        // 备注
-        transferInfo.setMemo("备注：" + new Date());
-        // 设置权限ID
-        transferInfo.setPermissionId(2);
-
-        // 参考区块
-        Chain.Block refBlock = wrapper.getNowBlock();
-        // trc20 转账处理器
-        Trc20TransferHandler trc20TransferHandler = new Trc20TransferHandler();
-        // 构造本地交易
-        Chain.Transaction transaction = trc20TransferHandler.buildLocalTransfer(transferInfo, refBlock.getBlockHeader());
-        // 账号1签名
-        Chain.Transaction signTransaction = wrapper.signTransaction(transaction, account1.getKeyPair());
-        // 账号2签名
-        signTransaction = wrapper.signTransaction(signTransaction, account2.getKeyPair());
         // 广播并返回ID
         String tid = wrapper.broadcastTransaction(signTransaction);
         log.debug(tid);
