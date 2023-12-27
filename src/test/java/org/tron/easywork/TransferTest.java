313a685753667a3e44214f568a2e6e35045fd03a
Aiden-777 <tianfangdao@gmail.com>
2023-12-28 02:03:55 +0800

    补充测试用例：小数点数值转账

--- src/test/java/org/tron/easywork/TransferTest.java
+++ src/test/java/org/tron/easywork/TransferTest.java
@@ -2,13 +2,21 @@ package org.tron.easywork;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.tron.trident.abi.TypeReference;
+import org.tron.trident.abi.datatypes.Address;
+import org.tron.trident.abi.datatypes.Bool;
+import org.tron.trident.abi.datatypes.Function;
+import org.tron.trident.abi.datatypes.generated.Uint256;
 import org.tron.trident.core.contract.Trc20Contract;
 import org.tron.trident.core.exceptions.IllegalException;
+import org.tron.trident.core.transaction.TransactionBuilder;
 import org.tron.trident.proto.Chain;
 import org.tron.trident.proto.Response;
 import org.tron.trident.utils.Convert;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Arrays;
 
 /**
  * @author Admin
@@ -61,6 +69,45 @@ public class TransferTest extends BaseTest {
         Chain.Transaction signTransaction = wrapper.signTransaction(transfer);
         // 广播
         String tid = wrapper.broadcastTransaction(signTransaction);
+        log.debug(tid);
+    }
+
+
+    /**
+     * trc20 转账 - trident原生 - 非本地构造交易
+     * 小数点转账，复制出trident源码，加以修改
+     */
+    @Test
+    public void transferTrc20_trident_custom() {
+        // 真实金额 - 单位个
+        String realAmount = "2.333";
+
+        // 根据合约地址获取合约信息
+        org.tron.trident.core.contract.Contract contract = wrapper.getContract(contractAddress);
+        // 构造trc20合约
+        Trc20Contract trc20Contract = new Trc20Contract(contract, from, wrapper);
+
+        // 精度
+        int decimals = trc20Contract.decimals().intValue();
+        // 手续费
+        long feeLimit = Convert.toSun("50", Convert.Unit.TRX).longValue();
+        // 实际转账金额（trc20不可分割单位）
+        BigInteger transferAmount = new BigDecimal(realAmount)
+                .multiply(BigDecimal.TEN.pow(decimals))
+                .toBigInteger();
+
+        Function transfer = new Function("transfer",
+                Arrays.asList(new Address(to),
+                        new Uint256(transferAmount)),
+                Arrays.asList(new TypeReference<Bool>() {
+                }));
+
+        TransactionBuilder builder = wrapper.triggerCall(from, contractAddress, transfer);
+        builder.setFeeLimit(feeLimit);
+        builder.setMemo("备注");
+
+        Chain.Transaction signedTxn = wrapper.signTransaction(builder.build());
+        String tid = wrapper.broadcastTransaction(signedTxn);
         log.debug(tid);
     }
 
